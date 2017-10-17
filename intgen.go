b223a85adcb1bb5730ca6359ce835a5b9f41f483
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-17 05:24:17 -0300

    added int gen

---
+++ intgen.go
@@ -0,0 +1,102 @@
+package iter
+
+//Integer Generator
+type  Generator struct {
+    start, stop, step, v int
+    HasNext              bool
+    fromValues           bool
+    values               []int
+}
+
+func NewGenerator(args ...int) *Generator {
+    self := &Generator{}
+
+    if len(args) == 1 {
+        self.start, self.stop, self.step = 0, args[0], 1
+    } else if len(args) == 2 {
+        self.start, self.stop, self.step = args[0], args[1], 1
+    } else if len(args) == 3 {
+        self.start, self.stop, self.step = args[0], args[1], args[2]
+    }
+
+    self.v = self.start - self.step
+    self.updateNext(self.start)
+    return self
+}
+
+func NewGeneratorOfVals(args ...int) *Generator {
+    self := &Generator{}
+    self.values = args
+
+    if len(self.values) > 0 {
+        self.start = 0
+        self.stop = len(self.values)
+        self.step = 1
+    }
+
+    self.fromValues = true
+    self.v = self.start - self.step
+    self.updateNext(self.start)
+    return self
+}
+
+func (gen *Generator) updateNext(v int) {
+    if gen.step > 0 {
+        gen.HasNext = v < gen.stop
+    } else {
+        gen.HasNext = v > gen.stop
+    }
+}
+
+func (gen *Generator) Val() int {
+
+    gen.v += gen.step
+
+    if (gen.step > 0  && gen.v >= gen.stop) ||
+        (gen.step < 0  && gen.v <= gen.stop) {
+        panic("generator out of range")
+    }
+
+    gen.updateNext(gen.v + gen.step)
+
+    if gen.fromValues {
+        return gen.values[gen.v]
+    }
+
+    return gen.v
+}
+
+func (gen *Generator) Values() []int {
+    var vals = make([]int, 0)
+    for gen.HasNext {
+        vals = append(vals, gen.Val())
+    }
+    return vals
+}
+
+func (gen *Generator) First() (int, bool) {
+    if !(gen.HasValues()) {
+        return 0 , false
+    }
+    return gen.start, true
+}
+
+func (gen *Generator) Last() (int, bool) {
+    if !(gen.HasValues()) {
+        return 0 , false
+    }
+
+    size := (gen.stop - gen.start) / gen.step
+    end := gen.start + (size * gen.step)
+    if end == gen.stop {
+        end = end - gen.step
+    }
+    return end, true
+}
+
+func (gen *Generator) HasValues() bool{
+    diff := gen.stop - gen.start
+    return (diff != 0) && (
+        (diff < 0 && gen.step < 0) || (diff > 0 && gen.step > 0))
+}
+
---
+++ intgen_test.go
@@ -0,0 +1,106 @@
+package iter
+
+import (
+    "testing"
+    "github.com/franela/goblin"
+)
+
+func TestIntGen(t *testing.T) {
+    g := goblin.Goblin(t)
+    g.Describe("IntGen", func() {
+        rng := []int{4, 6, 8, 9, 6, 7}
+        var val int
+        var ok bool
+        g.It("int iter range", func() {
+            var gen = NewGenerator(10)
+            val, ok = gen.First()
+            g.Assert(ok).IsTrue()
+            g.Assert(val).Equal(0)
+
+            val, ok = gen.Last()
+            g.Assert(val).Equal(9)
+            g.Assert(ok).IsTrue()
+
+            for i := 0; gen.HasNext && i < 10; i++ {
+                g.Assert(gen.Val()).Equal(i)
+            }
+
+            gen = NewGenerator(0, 10)
+            val, ok = gen.First()
+            g.Assert(ok).IsTrue()
+            g.Assert(val).Equal(0)
+
+            val, ok = gen.Last()
+            g.Assert(val).Equal(9)
+            g.Assert(ok).IsTrue()
+            for i := 0; gen.HasNext && i < 10; i++ {
+                g.Assert(gen.Val()).Equal(i)
+            }
+
+            gen = NewGenerator(1, 10, 2)
+            val, ok = gen.First()
+            g.Assert(ok).IsTrue()
+            g.Assert(val).Equal(1)
+
+            val, ok = gen.Last()
+            g.Assert(val).Equal(9)
+            g.Assert(ok).IsTrue()
+
+            for i := 1; gen.HasNext && i < 10; i += 2 {
+                g.Assert(gen.Val()).Equal(i)
+            }
+
+            gen = NewGenerator(1, 10, 2)
+            g.Assert(gen.Values()).Eql([]int{1, 3, 5, 7, 9})
+
+            gen = NewGenerator(10, 0, -3)
+            val, ok = gen.First()
+            g.Assert(ok).IsTrue()
+            g.Assert(val).Equal(10)
+
+            val, ok = gen.Last()
+            g.Assert(val).Equal(1)
+            g.Assert(ok).IsTrue()
+
+            for i := 10; gen.HasNext && i > 0; i -= 3 {
+                g.Assert(gen.Val()).Equal(i)
+            }
+
+            gen = NewGenerator(10, 0, 3)
+            g.Assert(gen.Values()).Eql([]int{})
+            gen = NewGenerator(0, 10, -3)
+            g.Assert(gen.Values()).Eql([]int{})
+
+            g.Assert(gen.HasValues()).IsFalse()
+
+            val, ok = gen.First()
+            g.Assert(ok).IsFalse()
+            g.Assert(val).Equal(0)
+
+            val, ok = gen.Last()
+            g.Assert(val).Equal(0)
+            g.Assert(ok).IsFalse()
+
+        })
+
+        g.It("int iter as values", func() {
+            var gen = NewGeneratorOfVals(rng...)
+            for i := 0; gen.HasNext; i++ {
+                g.Assert(gen.Val()).Equal(rng[i])
+            }
+        })
+        g.It("int iter as values should panic", func() {
+            var gen = NewGeneratorOfVals([]int{0, 1, 2}...)
+            defer func() {
+                r := recover()
+                g.Assert(r != nil).IsTrue()
+            }()
+            for i := 0; i < 10; i++ {
+                g.Assert(gen.Val()).Equal(i)
+            }
+        })
+    })
+
+}
+
+
